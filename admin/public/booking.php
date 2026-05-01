8227662dce8de6a37c60d4a7bb28856d07f39910
Rafli Aulia Al Giffari <fliaulia28@gmail.com>
2026-05-01 22:45:40 +0700

    folder tertinggal

--- admin/public/booking.php
+++ admin/public/booking.php
@@ -9,7 +9,7 @@ $data_booking = [
 ?>
 
 <!DOCTYPE html>
-<html lang="id">
+<html lang="id"><p></p>
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
